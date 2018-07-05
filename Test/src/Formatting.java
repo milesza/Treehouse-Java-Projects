9fc4680c74ea06bd836d1cedd41a5979e9ff1275
Zach Miles <zach.miles.developer@gmail.com>
2018-07-05 17:52:09 -0500

    Java Review of fundamentals

---
+++ Test/src/Formatting.java
@@ -0,0 +1,11 @@
+
+public class Formatting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String output = "Name: Bob Davis Major: Math";
+		System.out.print("\u001B[1m I am bold");
+	}
+
+}
